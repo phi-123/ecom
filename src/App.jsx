e048ca96927cd7549a4f98f967b55eec06d775e7
tên <email>
2023-10-30 18:12:56 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Gala from "./pages/Gala/Gala";
 import Search from "./pages/search/Search";
 import Category from "./pages/category/Category";
 import Home from "./pages/home/Home";
@@ -7,6 +8,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Gala />} />
       <Route path="/home" element={<Home />} />
       <Route path="/search" element={<Search />} />
       <Route path="/category" element={<Category />} />
---
+++ src/pages/Gala/Gala.jsx
@@ -0,0 +1,399 @@
+import "./home2.css";
+function Gala() {
+  return (
+    <div>
+      <div className="Home">
+        <div className="Navbar">
+          <div className="Menu">
+            <p className="rect1" />
+            <p className="rect2" />
+            <p className="rect3" />
+          </div>
+          <div className="logo">
+            <img src="/src/assets/image/logo2.jpg" alt="" className="Logo" />
+          </div>
+          <div className="srcbar">
+            <img className="icon" src="/src/assets/image/src.jpg" />
+            <input
+              className="icon1"
+              type="text"
+              placeholder="Search food ..."
+            />
+          </div>
+          <div className="sign">
+            <img className="Signin" src="/src/assets/image/sign.jpg" />
+            <p className="text">Sign In</p>
+          </div>
+          <div className="wishlist">
+            <img className="Wishlist" src="/src/assets/image/wish.jpg" />
+            <p className="text1">Wishlist</p>
+          </div>
+          <div className="cart">
+            <img className="Cart" src="/src/assets/image/cart.jpg" />
+            <p className="text2">$0.00</p>
+          </div>
+        </div>
+
+        <div className="over">
+          <p className="over1">Looking for Good Quality Ingredients?</p>
+          <button className="over2" type="reset">
+            Order Online
+          </button>
+        </div>
+        <div className="tional">
+          <div className="tional1">
+            <p className="tio">Buy 5 or more Save $1 each</p>
+            <p className="tio1"> Shop Now &gt; </p>
+          </div>
+          <div className="tional2">
+            <p className="tio">Collect 10x Point for Free Delivery</p>
+            <p className="tio1"> Shop Now &gt; </p>
+          </div>
+          <div className="tional3">
+            <p className="tio">50% Off for Fresh Vegetables</p>
+            <p className="tio1"> Shop Now &gt; </p>
+          </div>
+        </div>
+        <div className="reandpe6">
+          <div className="hg1">
+            <a className="hs2">Featured Items </a>
+            <a className="hs3">Show All</a>
+          </div>
+          <div className="hg2">
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$4.89</p>
+              <p className="name">Cavendish Banana</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$1.29</p>
+              <p className="name">Slicing Tomatoes</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$4.23</p>
+              <p className="name">Chiitos Crunch Chocolate Chips</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$0.89</p>
+              <p className="name">Goriorio Original Crackers</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$5.75</p>
+              <p className="name">Yve Large Brown Eggs, 12 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$4.23</p>
+              <p className="name">Jemima Waffle</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+          </div>
+          <button type="reset" className="gf1">
+            &gt;
+          </button>
+        </div>
+        {/* -------------------------- */}
+        <div className="reandpe7">
+          <div className="hg1">
+            <a className="hs2">Best Seller in Your Area</a>
+            <a className="hs3">Show All</a>
+          </div>
+          <div className="hg2">
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Sunmaid Frozz</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Alkaline Battery</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$1.89</p>
+              <p className="name">Maskara Cakeps</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$9.21</p>
+              <p className="name">Detergent Lotto</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Sunmaid Frozz</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Alkaline Battery</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+          </div>
+          <button type="reset" className="gf1">
+            &gt;
+          </button>
+        </div>
+        {/* ---------------------------------------- */}
+        <div className="reandpe8">
+          <div className="hg1">
+            <a className="hs2">Discount This Week</a>
+            <a className="hs3">Show All</a>
+          </div>
+          <div className="hg2">
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$5.23</p>
+              <p className="name">Gala Apples</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$5.23</p>
+              <p className="name">Apple Jumbo</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$5.23</p>
+              <p className="name">Gala Apples</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Apple Jumbo</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$5.23</p>
+              <p className="name">Gala Apples</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <div className="hqe1">
+                <p className="ga4">-15%</p>
+                <p className="ga5">$7.12</p>
+              </div>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Apple Jumbo</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+          </div>
+          <button type="reset" className="gf1">
+            &gt;
+          </button>
+        </div>
+        <div className="dowload">
+          <p className="dow1">Start your day with Fresh Foods</p>
+          <div className="dow2">
+            <button className="dow3" type="reset">
+              Google Play Store
+            </button>
+            <button className="dow4" type="reset">
+              Apple Store
+            </button>
+            <img src="/src/assets/image/Iphone.jpg" className="dow5" />
+          </div>
+        </div>
+        <div className="reandpe4">
+          <div className="hg1">
+            <a className="hs2">Fresh Vegetables</a>
+            <a className="hs3">Show All</a>
+          </div>
+          <div className="hg2">
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Mushroom</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Cabbages</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$1.89</p>
+              <p className="name">Mushroom</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$9.21</p>
+              <p className="name">Cabbages</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Mushroom</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Cabbages</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+          </div>
+          <button type="reset" className="gf1">
+            &gt;
+          </button>
+        </div>
+        <div className="reandpe5">
+          <div className="hg1">
+            <a className="hs2">Dairy & Milk</a>
+            <a className="hs3">Show All</a>
+          </div>
+          <div className="hg2">
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Eggland White 4 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Yve Large Brown Eggs 12 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$1.89</p>
+              <p className="name">Eggland White 4 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$9.21</p>
+              <p className="name">Yve Large Brown Eggs 12 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.39</p>
+              <p className="name">Eggland White 4 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+            <div className="card3 ok">
+              <img src="/src/assets/image/im.jpg"></img>
+              <p className="Do-la">$2.11</p>
+              <p className="name">Yve Large Brown Eggs 12 Count</p>
+              <button type="reset">Add to Cart</button>
+            </div>
+          </div>
+          <button type="reset" className="gf1">
+            &gt;
+          </button>
+        </div>
+        <div className="Sop">
+          <div className="hg1">
+            s<a className="hs2">Shop By Department</a>
+            <a className="hs3">Show All Department</a>
+          </div>
+          <div className="Sop1">
+            <div>
+              <p className="tron"></p>
+              <p className="New1">New Arrivals</p>
+            </div>
+            <div>
+              <p className="tron"></p>
+              <p className="New1">Dairy & MIlk</p>
+            </div>
+            <div>
+              <p className="tron"></p>
+              <p className="New1">Fresh Produce</p>
+            </div>
+            <div>
+              <p className="tron"></p>
+              <p className="New1"> Meat</p>
+            </div>
+            <div>
+              <p className="tron"></p>
+              <p className="New1"> Snacks</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <div className="footer5">
+          <div className="Groud477">
+            <img src="/src/assets/image/logo2.jpg" alt="" className="Logo1" />
+            <img className="a1a" src="/src/assets/image/dc.jpg"></img>
+            <p className="ph1">221 B Santa Monica, Los Angeles</p>
+            <img className="a2a" src="/src/assets/image/te.jpg"></img>
+            <p className="ph2">(+1) 923 2341 22</p>
+            <img className="a3a" src="/src/assets/image/gm.jpg"></img>
+            <p className="ph3">Contact@tanahairstudio.com</p>
+            <p className="gg">@2020 TanahAir Studio. All rights reserved.</p>
+          </div>
+          <div className="Category">
+            <p className="c1c">Categories</p>
+            <p className="c0c">Dairy & Milk</p>
+            <p className="c0c">Fresh Produce</p>
+            <p className="c0c">Meat</p>
+            <p className="c0c">Snacks</p>
+            <p className="c0c">Frozen</p>
+            <p className="c0c">Vegetables</p>
+          </div>
+          <div className="Company">
+            <p className="b1b">Company</p>
+            <p className="b0b">About Us</p>
+            <p className="b0b">Contact Us</p>
+            <p className="b0b">Privacy & Poilicy</p>
+            <p className="b0b">Payment Method</p>
+          </div>
+          <div className="join">
+            <p className="Join">Join Our Newsletter</p>
+            <p className="em">Your Email</p>
+            <input className="gml" type="text" placeholder="Enter Your Email" />
+            <button className="re1" type="reset">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Gala;
